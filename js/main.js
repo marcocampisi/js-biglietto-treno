64caeb1361ec3c37f3b6f2681d9191a4a46423e7
marcocampisi <marco.campisi@outlook.it>
2023-06-12 14:22:22 +0200

    Imported resources

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const tariffaKm = 0.21;
+const spanCosto = document.getElementById('#costo');
+
+alert('Benvenuto, inserisci la tua età e quanti KM devi percorrere.')
+
+let age = prompt("Età: ");
+let userKm = prompt("KM: ");
+
