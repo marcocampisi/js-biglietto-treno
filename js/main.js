661e1c5ed0e60e7549ea5c3251d532c7dadc5814
marcocampisi <marco.campisi@outlook.it>
2023-06-12 14:25:29 +0200

    Fixed background

--- js/main.js
+++ js/main.js
@@ -6,3 +6,6 @@ alert('Benvenuto, inserisci la tua età e quanti KM devi percorrere.')
 let age = prompt("Età: ");
 let userKm = prompt("KM: ");
 
+if (age < 18) {
+    spanCosto.innerHTML = (tariffaKm * userKm) - 20%;
+}
