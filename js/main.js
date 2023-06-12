3d7ca5b339c4c7bcdc7abbf19a6d4b980f093004
marcocampisi <marco.campisi@outlook.it>
2023-06-12 14:46:25 +0200

    Fixed script

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ const tariffaKm = 0.21;
 const spanCosto = document.getElementById('costo');
 
 alert('Benvenuto, inserisci la tua età e quanti KM devi percorrere.');
-let age = parseInt(prompt("Età: "));
-let userKm = parseInt(prompt("KM: "));
+let age = parseFloat(prompt("Età: "));
+let userKm = parseFloat(prompt("KM: "));
 let price = (tariffaKm * userKm).toFixed(2);
 
 if (age < 18) {
@@ -11,5 +11,5 @@ if (age < 18) {
 } else if (age > 65) {
     spanCosto.innerHTML = (price) - (price * .40) + '€';
 } else {
-    spanCosto.innerHTML = price.toFixed(2) + '€';
+    spanCosto.innerHTML = price + '€';
 }
